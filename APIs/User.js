cd71d4f61aed5675322ccc8676299ce80740c64c
shabushabushabu <enigma14389@gmail.com>
2022-04-25 02:12:32 +0700

    fix date parsing bug and update user model

--- APIs/User.js
+++ APIs/User.js
@@ -1,5 +1,6 @@
-const sha1= require('sha1');
+const sha1 = require('sha1');
 const crypto = require('crypto');
+const moment = require('moment');
 
 const UserModel = require("../Models/User");
 
@@ -9,8 +10,8 @@ const GetUserHandler = async (req, res) => {
 
     const userEmail = req.params.email;
 
-    const docs = await UserModel.User.findOne({email: userEmail})
-    if (docs){
+    const docs = await UserModel.User.findOne({ email: userEmail })
+    if (docs) {
         console.log("Get user details")
         res.send(JSON.stringify({
             id: docs.id,
@@ -18,9 +19,9 @@ const GetUserHandler = async (req, res) => {
             email: docs.email,
             birthdate: docs.birthdate,
             occupation: docs.occupation
-        }));  
+        }));
 
-    } else{
+    } else {
         console.log("User not exist")
         res.sendStatus(404);
     }
@@ -33,22 +34,26 @@ const PostUserHandler = async (req, res) => {
     // check existing users - callback , promise (then...), await
     const userEmail = req.body.email;
 
-    const docs = await UserModel.User.find({email: userEmail})
+    const docs = await UserModel.User.find({ email: userEmail })
     console.log(docs)
     if (docs.length > 0) {
         console.log("User Existed")
         res.sendStatus(403)
-        return   
+        return
     } else {
         console.log("Create new user")
         const salt = crypto.randomBytes(128).toString('base64');
         const newUser = UserModel.User({
-            name: req.body.name,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
             email: userEmail,
-            birthdate: new Date(req.body.birthdate),
+            birthdate: moment(req.body.birthdate).utcOffset(0)
+            .set({ hour: 0, minute: 0, second: 0, millisecond: 0 }).format(),
             occupation: req.body.occupation,
             hashPassword: sha1(req.body.password + salt),
-            salt: salt
+            salt: salt,
+            activateDatetime: Date.now(),
+            status: "active"
         });
 
         console.log("Add new user to DB")
@@ -68,8 +73,8 @@ const PutUserDetailsHandler = async (req, res) => {
     const userEmail = req.params.email;
 
     const updateUser = {
-        name: req.body.name, 
-        birthdate: new Date(req.body.birthdate), 
+        name: req.body.name,
+        birthdate: new Date(req.body.birthdate),
         occupation: req.body.occupation
     };
     // const docs = await UserModel.User.findOne({email: userEmail}) // TODO is this necessary - perf
@@ -88,22 +93,22 @@ const PutUserDetailsHandler = async (req, res) => {
     //     console.log("User not exist");
     //     res.sendStatus(404);
     // }
-    const result = await UserModel.User.findOneAndUpdate({email: userEmail}, updateUser);
+    const result = await UserModel.User.findOneAndUpdate({ email: userEmail }, updateUser);
     console.log(result)
     res.send(JSON.stringify({
         "result": result
     }));
-    
+
 
 }
 
 const DeleteUserHandler = async (req, res) => {
     console.log("DELETE /user/:email");
     console.log(req.body);
-    
+
     const userEmail = req.params.email;
 
-    const user = await UserModel.User.findOne({email: userEmail})
+    const user = await UserModel.User.findOne({ email: userEmail })
     if (user) {
         const hashPassword = user.hashPassword; // confirm password
         const salt = user.salt;
@@ -111,7 +116,7 @@ const DeleteUserHandler = async (req, res) => {
         const attemptPassword = req.body.password
         if (hashPassword == sha1(attemptPassword + salt)) {
             // delete user
-            const result = await UserModel.User.deleteOne({email: userEmail})
+            const result = await UserModel.User.deleteOne({ email: userEmail })
             res.send({
                 "message": "success",
                 "status": "user deleted"
--- Models/User.js
+++ Models/User.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: String,
+    firstName: String,
+    lastName: String,
     email: String,
     birthdate: Date,
     occupation: String,
     hashPassword: String,
-    salt: String
+    salt: String,
+    activateDatetime: Date,
+    status: String
   });
 
 const User = mongoose.model("User", userSchema);
