84646249f031a7fdff8dc63b23aa5ec22c57c46e
Titaporn Fongwattanakul <shabushabu@Titaporns-Air.ST-244F>
2022-05-16 22:31:23 +0700

    refactor login API

--- APIs/Authentication.js
+++ APIs/Authentication.js
@@ -6,30 +6,32 @@ const UserModel = require("../Models/User");
 const LoginHandler = async (req, res) => {
     console.log("POST /login");
     console.log(req.body);
+    try {
+        const user = await UserModel.User.findOne({email: req.body.email});
+        console.log(user);
+        if (user) {
+            const hashPassword = user.hashPassword;
+            const salt = user.salt;
 
-    const userEmail = req.body.email;
-    const user = await UserModel.User.findOne({email: userEmail})
-    console.log(user)
-    if (user) {
-        const hashPassword = user.hashPassword;
-        const salt = user.salt;
-
-        const attemptPassword = req.body.password
-        if (hashPassword == sha1(attemptPassword + salt)) {
-            res.send({
-                "message": "success",
-                "token": sha1(JSON.stringify({
-                    user: user.name,
-                    email: user.email
-                }))
-            });
+            const attemptPassword = req.body.password;
+            if (hashPassword == sha1(attemptPassword + salt)) {
+                res.send({
+                    "message": "success",
+                    "token": sha1(JSON.stringify({
+                        user: user.name,
+                        email: user.email
+                    }))
+                });
+            } else {
+                res.sendStatus(401);
+            }
         } else {
-            console.log("Incorrect password");
-            res.sendStatus(401);
+            res.sendStatus(404);
         }
-    } else {
-        console.log("Email not found");
-        res.sendStatus(401) ;
+
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(400);
     }
 }
 
